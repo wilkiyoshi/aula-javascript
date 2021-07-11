a55c7def349432cddcf9e0d1b8086aa486c9f969
William Kiyoshi Taniguchi <wilkiyoshi>
2021-07-10 23:55:52 -0300

    atualização de arquivos

--- scripts.js
+++ scripts.js
@@ -104,3 +104,31 @@ console.log("Resto=" + (x%=y));
 // && ou AND (e)
 // || ou OR (ou)
 // ! (negação)
+
+
+///////////////PRÁTICA CONDICIONAIS//////////////
+console.log("condicionais")
+var num = 1;
+num = 5 ;
+
+if (num === 1){
+    console.log("num é igual a 1")
+}else if (num === 2){
+    console.log("num é igual a 2")}
+    else{
+        console.log("num é 5")        
+}
+
+//switch case
+var mes = "janeiro"
+
+switch (mes){
+    case "fevereiro": console.log("mes 2")
+    break;
+    case "março": console.log("mes 3")
+    break;
+    case "janeiro": console.log("mes 1")
+    break;
+    default: console.log("nenhum dos casos atendidos")
+}
+
